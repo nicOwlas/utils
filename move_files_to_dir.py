ec72bff62a34468667711eca0db8171142e10a84
Nicolas Draber <nicolas.draber@gmail.com>
2022-08-24 17:38:34 +0200

    Add option to select the file among duplicates to keep

--- move_files_to_dir.py
+++ move_files_to_dir.py
@@ -5,7 +5,7 @@ import pathlib
 # Priority 1: extension is in list of valid extensions (do not move .lua, .lrdata)
 # Priority 2: ignore when there are 2 duplicates and one is in #recycle
 # Priority 3: keep the file with the shortest path
-def filter_files(duplicated_files, extensions_watched):
+def filter_files(duplicated_files, extensions_watched, keep_file_with_longest_path):
     files_to_move = []
     for k in duplicated_files["duplicates"].keys():
         # Remove from list the files with #recycle in its path
@@ -13,7 +13,11 @@ def filter_files(duplicated_files, extensions_watched):
             file for file in duplicated_files["duplicates"][k] if "#recycle" not in file
         ]
         # Sort the list of files with the shortest path first
-        duplicated_sorted_files = sorted(files_outside_bin, key=len)
+        duplicated_sorted_files = sorted(
+            files_outside_bin,
+            key=len,
+            reverse=parse_boolean(keep_file_with_longest_path),
+        )
 
         # Consider moving files if at least one element is in the list
         if len(duplicated_sorted_files) > 1:
@@ -21,7 +25,18 @@ def filter_files(duplicated_files, extensions_watched):
                 filename, file_extension = os.path.splitext(file)
                 if file_extension in extensions_watched:
                     files_to_move.append(file)
+    print("Files to move: \n", json.dumps(files_to_move, indent=4, sort_keys=True))
     return files_to_move
+
+
+def parse_boolean(string):
+    return string.lower() in [
+        "true",
+        "1",
+        "t",
+        "y",
+        "yes",
+    ]
 
 
 def move_files(files_to_move, destination):
@@ -41,6 +56,7 @@ def move_files(files_to_move, destination):
 if __name__ == "__main__":
     input_file = sys.argv[1]
     destination = sys.argv[2]
+    keep_file_with_longest_path = sys.argv[3]
     extensions_watched = {
         ".WAV",
         ".JPEG",
@@ -58,8 +74,8 @@ if __name__ == "__main__":
         ".mov",
         ".png",
         ".heic",
+        ".THM",
     }
-    # "/Users/nicolas/Downloads/2022-05-02_duplicates-test.json"
     with open(input_file, "r") as f:
         duplicated_files = json.load(f)
 
@@ -67,5 +83,7 @@ if __name__ == "__main__":
         "Number of hash with duplicates: ", len(duplicated_files["duplicates"].keys())
     )
 
-    files_to_move = filter_files(duplicated_files, extensions_watched)
+    files_to_move = filter_files(
+        duplicated_files, extensions_watched, keep_file_with_longest_path
+    )
     move_files(files_to_move, destination)
