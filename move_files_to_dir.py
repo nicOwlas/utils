eb582c074be7b4b00d8ab25020b41ad3cb0d688c
Nicolas Draber <nicolas.draber@gmail.com>
2023-01-29 21:01:39 -0400

    Keep file among duplicates even if its in the bin

--- move_files_to_dir.py
+++ move_files_to_dir.py
@@ -14,7 +14,9 @@ def filter_files(duplicated_files, extensions_watched, keep_file_with_longest_pa
     for k in duplicated_files["duplicates"].keys():
         # Remove from list the files with #recycle in its path
         files_outside_bin = [
-            file for file in duplicated_files["duplicates"][k] if "#recycle" not in file
+            file
+            for file in duplicated_files["duplicates"][k]
+            if "blablibloblu" not in file
         ]
         # Sort the list of files with the shortest path first
         duplicated_sorted_files = sorted(
@@ -26,7 +28,7 @@ def filter_files(duplicated_files, extensions_watched, keep_file_with_longest_pa
         # Consider moving files if at least one element is in the list
         if len(duplicated_sorted_files) > 1:
             for file in duplicated_sorted_files[1:]:
-                filename, file_extension = os.path.splitext(file)
+                _, file_extension = os.path.splitext(file)
                 if file_extension in extensions_watched:
                     files_to_move.append(file)
     print("Files to move: \n", json.dumps(files_to_move, indent=4, sort_keys=True))
@@ -50,10 +52,10 @@ def move_files(files_to_move, destination):
         pathlib.Path(os.path.dirname(destination_path)).mkdir(
             parents=True, exist_ok=True
         )
-        print("Moving file #{}: {}".format(count, destination_path))
+        print(f"Moving file #{count}: {destination_path}")
         try:
             shutil.move(file, destination_path)
-        except:
+        except FileNotFoundError:
             print("File not found")
 
 
@@ -80,7 +82,7 @@ if __name__ == "__main__":
         ".heic",
         ".THM",
     }
-    with open(input_file, "r") as f:
+    with open(input_file, "r", encoding="utf-8") as f:
         duplicated_files = json.load(f)
 
     print(
