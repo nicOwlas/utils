6bccb635b6c6b38ed5bbdfbbe5151a432a6ca1de
Nicolas Draber <nicolas.draber@gmail.com>
2023-03-09 12:14:33 +0100

    Add perceptual hash

--- file_deduplication/file_hash.py
+++ file_deduplication/file_hash.py
@@ -1,4 +1,11 @@
 import hashlib
+import pathlib
+
+import imagehash
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
 
 
 def hash_bytestr_iter(bytesiter, hasher, ashexstr=False):
@@ -16,9 +23,20 @@ def file_as_blockiter(afile, blocksize=65536):
 
 
 def hexhash(file: str):
-    """Return file hash"""
+    """Return file sha256 hash"""
     return hash_bytestr_iter(
         file_as_blockiter(open(file, "rb")),
         hashlib.sha256(),
         ashexstr=True,
+    )
+
+
+def dhash(file: str):
+    """Return 16 bit perceptual hash of an image file (JPG, TIFF, HEIC, PNG)"""
+    ext = pathlib.Path(file).suffix.lower()
+    if ext not in [".jpg", ".jpeg", ".png", ".heic"]:
+        raise ValueError(f"{ext} file type is not supported")
+    return imagehash.dhash(
+        Image.open(file),
+        hash_size=16,
     )
