d917f6b6da3c1894f5a18fb78e5f1713b0f6a8b8
Nicolas Draber <nicolas.draber@gmail.com>
2023-03-09 20:46:12 +0100

    Check if for a given row, dhash is empty

--- file_deduplication/db_operations.py
+++ file_deduplication/db_operations.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from file_hash import hexhash
+from file_hash import dhash, hexhash
 
 
 def create_db(db_name: str):
@@ -36,7 +36,32 @@ def insert_db_entry(file_path: str, cursor) -> None:
         pass
 
 
+def is_dhash_null(connection, file_path: str):
+    """Returns True if a given row dhash value is NULL """
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT CASE WHEN dhash IS NULL THEN 1 ELSE 0 END FROM pictures WHERE path = ?",
+        (file_path,),
+    )
+
+    row = cursor.fetchone()
+
+    # Check if dhash is empty
+    if row is not None and row[0] == 1:
+        return True
+    else:
+        return False
+
+
+def update_dhash(file_path: str, cursor) -> None:
+    """Add an entry to the DB"""
+    cursor.execute(
+        "UPDATE pictures SET dhash = ? WHERE path = ? AND dhash IS NULL",
+        (dhash(file_path), file_path),
+    )
+
+
 def read_db_entry(cursor):
     """Display DB content"""
-    rows = cursor.execute("SELECT path, secret FROM pictures").fetchall()
+    rows = cursor.execute("SELECT path, hash, dhash FROM pictures").fetchall()
     print(rows)
