01e9863043de4dfdea8d16aae3684f66c1b92e44
Nicolas Draber <nicolas.draber@gmail.com>
2023-03-13 10:59:15 +0100

    populate the ascii_path column

---
+++ file_deduplication/populate_ascii_path_column.py
@@ -0,0 +1,46 @@
+import sqlite3
+import sys
+
+from unidecode import unidecode
+
+
+def ascii_path(root_path: str, db_name: str) -> None:
+    """Remove non existing files"""
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    # Find duplicated hashes
+    cursor.execute(
+        """
+        SELECT rowid, path, ascii_path
+        FROM pictures ORDER BY rowid
+    """
+    )
+    # Update ascii_path with its unidecode version
+    updated_path = 0
+    for index, row in enumerate(cursor.fetchall()):
+        rowid, path, ascii_path = row
+        unidecode_path = unidecode(path)
+        if ascii_path != unidecode_path:
+            updated_path += 1
+            print(f"#{index}: {path} -> {unidecode_path}")
+            cursor.execute(
+                """
+                UPDATE pictures
+                SET ascii_path = ?
+                WHERE rowid = ?
+                """,
+                (unidecode_path, rowid),
+            )
+
+        if updated_path > 0 and updated_path % 100 == 0:
+            conn.commit()
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    root_path = sys.argv[1]
+    db_path = sys.argv[2]
+    ascii_path(root_path, db_path)
